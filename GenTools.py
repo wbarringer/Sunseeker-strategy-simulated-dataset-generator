34eae302b00b0e20495fd44c0bc69a08b20d8e3f
wbarringer <61356087+wbarringer@users.noreply.github.com>
2020-02-22 16:11:13 -0500

    End of day

--- GenTools.py
+++ GenTools.py
@@ -1,1 +1,24 @@
 # Tools for generating a simulation dataset that can be used to test and train strategy software
+
+# import random
+import pyodbc
+
+
+class TimeGenerator:
+
+    def __init__(self):
+        current_time = 0
+        time_step = 30  # Simulation time between data points in second
+
+    # def
+
+
+def write_database(server, database, table, data):
+    print("attempting to connect")
+    connection = pyodbc.connect("Driver={SQL Server};"
+                                "Server=" + server + ";"
+                                "Database=" + database + ";"
+                                "Trusted_Connection=yes;")
+    cursor = connection.cursor()
+    print("Connected")
+    cursor.execute("INSERT INTO " + database + ".dbo." + table, data)
---
+++ test.py
@@ -0,0 +1,27 @@
+# Test program for testing other tools
+
+import GenTools
+import pyodbc
+
+server = "CAE-FALL2019-CA\SUNSEEKER_Test"
+database = "DataGenerationTestDB"
+table = "Array"
+
+if __name__ == '__main__':
+    print("This worked, I guess")
+    GenTools.write_database(server, database, table, ('12:15:25', '615', '123', '5'))
+    # conn = pyodbc.connect("Driver={SQL Server};"
+    #                       "Server=CAE-FALL2019-CA\SUNSEEKER_Test;"
+    #                       "Database=DataGenerationTestDB;"
+    #                       "Trusted_Connection=yes;")
+    # cursor = conn.cursor()
+    # cursor.execute("""
+    #                 INSERT INTO DataGenerationTestDB.dbo.Array (Time, Array_Power, Array_Voltage, Array_Current)
+    #
+    #                 VALUES
+    #                 ('12:15:05', '600', '120', '5'),
+    #                 ('12:15:10', '605', '121', '5'),
+    #                 ('12:15:15', '610', '122', '5'),
+    #                 ('12:15:20', '615', '123', '5')
+    #                 """)
+    conn.commit()
